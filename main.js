698b184c312c23977459f7eb5307867dba7ef2cf
barbaramootian <barbaramootian@student.moringaschool.com>
2021-10-16 15:26:21 +0300

    add different files

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const firstName = 'Barbara'
+const lastName = 'Mootian'
+
+let val;
+
+// concatenate 
+val = firstName + '' + lastName
+//Append
+val = "Barbara"
+val += "Mootian"
+// Escaping
+
+//Length
+//concat
+console.log(val)
