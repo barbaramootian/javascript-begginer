7e388dbc26e935c3aa8d1205dcdc5d67e09a2312
barbaramootian <barbaramootian@student.moringaschool.com>
2021-10-16 16:28:25 +0300

    arrays

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
-const firstName = 'Barbara'
-const lastName = 'Mootian'
-
+//arrays
+const arr1 = [40, 60,80]
+const arr2 = new Array(90, 20)
+const users = ['Barbara', 'Reson', 'Mootian']
+const anything = [ 'hello', 2, true,
+{name: 'barbara', age:20},
+new Date()]
 let val;
-
-// concatenate 
-val = firstName + '' + lastName
-//Append
-val = "Barbara"
-val += "Mootian"
-// Escaping
-val =  "That great you are going to understand Js eithin one self tought week"
-//Length
-val = firstName.length
-//concat
-val = firstName.concat('',lastName)
-console.log(val)
-//change case
-val = firstName.toUpperCase()
-val = lastName.toLowerCase()
+//manipulating the array
+//add to the end
+arr1.push(200)
+//Add to the front
+arr1.unshift(300)
+//remove the end
+arr1.pop()
+//at position 1 , remove one item and insert "one" "two"
+arr1.splice(1, 2, "one", "two")
+console.log(arr1)
+// reverse 
+arr1.reverse
