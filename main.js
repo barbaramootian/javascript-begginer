b4cc888d17b88fa1d3dd4d84da3ac9c2e6dd23db
barbaramootian <barbaramootian@student.moringaschool.com>
2021-10-16 19:18:56 +0300

    add the if statement

--- main.js
+++ main.js
@@ -1,42 +1,51 @@
 
-//arrays
-const arr1 = [40, 60,80]
-const arr2 = new Array(90, 20)
-const users = ['Barbara', 'Reson', 'Mootian']
-const anything = [ 'hello', 2, true,
-{name: 'barbara', age:20},
-new Date()]
-let val;
-//manipulating the array
-//add to the end
-arr1.push(200)
-//Add to the front
-arr1.unshift(300)
-//remove the end
-arr1.pop()
-//at position 1 , remove one item and insert "one" "two"
-arr1.splice(1, 2,)
-// reverse 
-arr1.reverse
+// //arrays
+// const arr1 = [40, 60,80]
+// const arr2 = new Array(90, 20)
+// const users = ['Barbara', 'Reson', 'Mootian']
+// const anything = [ 'hello', 2, true,
+// {name: 'barbara', age:20},
+// new Date()]
+// let val;
+// //manipulating the array
+// //add to the end
+// arr1.push(200)
+// //Add to the front
+// arr1.unshift(300)
+// //remove the end
+// arr1.pop()
+// //at position 1 , remove one item and insert "one" "two"
+// arr1.splice(1, 2,)
+// // reverse 
+// arr1.reverse
 
-// concat array
-val =arr1.concat(arr2)
-//sort
-val  = users.sort ().reverse ()
-val  = arr1.sort()
-//sort with compare function 
-val = arr1.sort(function(a, b){
-    return a-b
-})
-// reverse sort with compare function 
-val = arr1.sort(function(a, b){
-    return b-a
-})
-// find
-function gt10(num){
-    return num > 10
+// // concat array
+// val =arr1.concat(arr2)
+// //sort
+// val  = users.sort ().reverse ()
+// val  = arr1.sort()
+// //sort with compare function 
+// val = arr1.sort(function(a, b){
+//     return a-b
+// })
+// // reverse sort with compare function 
+// val = arr1.sort(function(a, b){
+//     return b-a
+// })
+// // find
+// function gt10(num){
+//     return num > 10
+// }
+// val = arr1.find(gt10)
+// //find and filter
+// val = arr1.filter(gt10)
+// console.log(val)
+
+//if statement
+const mark ='5'
+// Abstract equallity comparision
+if (mark == 5){
+    console.log('The mark is 5')
+}else{
+    console.log('the mark is not 5')
 }
-val = arr1.find(gt10)
-//find and filter
-val = arr1.filter(gt10)
-console.log(val)
